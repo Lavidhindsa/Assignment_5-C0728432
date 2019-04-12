dbf2fc8cbd9603d8f39db360789717fc62c0791e
visualstudio <visualstudio@122-PC17>
2019-04-12 19:43:21 -0400

    Assignment_5_C0728432

--- Assignment_5 C0728432/Program.cs
+++ Assignment_5 C0728432/Program.cs
@@ -8,22 +8,28 @@ namespace Assignment_5_C0728432
 {
     class Program
     {
-        static void  Main(string[] args)
+        static void Main(string[] args)
         {
             Program a = new Program();
             Console.WriteLine("the longest word is : ");
             Console.WriteLine(a.MyFunctionA("Ada Lovelace wrote the first algorithm designed for processing by an Analytical Engine."));
         }
 
-        public int  MyFunctionA(string input)
+        //private bool MyFunctionA(string v)
+        //{
+        // throw new NotImplementedException();
+        //}
+        //manvir
+        public String MyFunctionA(string input)
         {
-            //Write a C# program to find the longes word in a string.
-            string[] words = input.Split('');
+            //Write a C# program to find the longest word in a string.
+            string[] words = input.Split(' ');
             int wordarrayLength = words.Length;
-
-            foreach (var wod in words)
+            String longestword = words[0];
+            int[] wordsLength = new int[words.Length];
+            for (int s = 0; s < words.Length; s++)
             {
-                //TODO
+                wordsLength[s] = words[s].Length;
             }
             // loop post condition: we now jave array wordsLength which contains the lengths of each word
 
@@ -33,10 +39,20 @@ namespace Assignment_5_C0728432
             for (int y = 0; y < words.Length - 1; y++)
             {
                 currentWord = words[y];
+
                 nextWord = words[y + 1];
+                if (words[y + 1].Equals(words[y]))
+                {
+                    longestword = words[y + 1];
+                }
+
                 // TODO :Use an IF Statementr to make sure thet variable longestWord is always set to the Longest Word in the input string
             }
+            return longestword;
 
         }
+
+
     }
 }
+
